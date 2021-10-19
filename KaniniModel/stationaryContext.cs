3338bd3506368798d818b5cab9db38bb5909419d
Chandru Shankar <88303166+ChandruShankar@users.noreply.github.com>
2021-10-19 21:29:12 +0530

    Update stationaryContext.cs

--- KaniniModel/stationaryContext.cs
+++ KaniniModel/stationaryContext.cs
@@ -31,7 +31,7 @@ namespace MovieApi.KaniniModel
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:stationarymovie.database.windows.net,1433;Initial Catalog=stationary;Persist Security Info=False;User ID=Team8;Password=Teammovie@8;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=tcp:movietickets.database.windows.net,1433;Initial Catalog=stationary;Persist Security Info=False;User ID=Team8;Password=Teammovie@8;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
