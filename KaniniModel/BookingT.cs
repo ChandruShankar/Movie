4892fbf5bf15e7723054e941d65f5a6df428ed26
Chandru Shankar <chandru.shankar@kanini.com>
2021-10-19 07:32:08 +0530

    first commit

---
+++ KaniniModel/BookingT.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MovieApi.KaniniModel
+{
+    public partial class BookingT
+    {
+        public string Id { get; set; }
+        public string MovieName { get; set; }
+        public int? Price { get; set; }
+        public string MovieDescription { get; set; }
+        public string MoviePoster { get; set; }
+    }
+}
